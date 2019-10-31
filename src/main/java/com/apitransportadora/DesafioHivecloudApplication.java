ed90599de3ae0dee0aa35a3f47774aa9bbe2faa7
renanavs <ravs@ecomp.poli.br>
2019-10-31 12:21:06 -0300

    first commit

---
+++ src/main/java/com/apitransportadora/DesafioHivecloudApplication.java
@@ -0,0 +1,13 @@
+package com.apitransportadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioHivecloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioHivecloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apitransportadora/DesafioHivecloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apitransportadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioHivecloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
